3023667f52ac659dfe613f5066e3925b470973ac
snapetech <keith@snape.tech>
2026-09-23 17:24:56 -0600

    fix: skip fresh author refreshes after book additions

--- src/NzbDrone.Core/Books/Handlers/BookAddedHandler.cs
+++ src/NzbDrone.Core/Books/Handlers/BookAddedHandler.cs
@@ -8,17 +8,27 @@ namespace NzbDrone.Core.Books
     public class BookAddedHandler : IHandle<BookAddedEvent>
     {
         private readonly IManageCommandQueue _commandQueueManager;
+        private readonly ICheckIfAuthorShouldBeRefreshed _checkIfAuthorShouldBeRefreshed;
 
-        public BookAddedHandler(IManageCommandQueue commandQueueManager)
+        public BookAddedHandler(IManageCommandQueue commandQueueManager,
+                                ICheckIfAuthorShouldBeRefreshed checkIfAuthorShouldBeRefreshed)
         {
             _commandQueueManager = commandQueueManager;
+            _checkIfAuthorShouldBeRefreshed = checkIfAuthorShouldBeRefreshed;
         }
 
         public void Handle(BookAddedEvent message)
         {
-            if (message.DoRefresh)
+            if (!message.DoRefresh)
             {
-                _commandQueueManager.Push(new RefreshAuthorCommand(message.Book.Author.Value.Id));
+                return;
+            }
+
+            var author = message.Book.Author.Value;
+
+            if (!author.LastInfoSync.HasValue || _checkIfAuthorShouldBeRefreshed.ShouldRefresh(author))
+            {
+                _commandQueueManager.Push(new RefreshAuthorCommand(author.Id));
             }
         }
     }
