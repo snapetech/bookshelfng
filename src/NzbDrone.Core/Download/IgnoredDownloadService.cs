cadf113a4f44713eff8bec02b7b9d0b517da2443
snapetech <keith@snape.tech>
2026-09-24 11:46:33 -0600

    chore(download): remove unused extension import

--- src/NzbDrone.Core/Download/IgnoredDownloadService.cs
+++ src/NzbDrone.Core/Download/IgnoredDownloadService.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using NLog;
-using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Download.TrackedDownloads;
 using NzbDrone.Core.Messaging.Events;
 
